1cf46f2f69f4ce0db345b9cea6a3f91833f00d8b
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-04-22 23:34:35 +0545

    Add a hook for fadein animation

---
+++ src/animation/useFadeInAnimation.ts
@@ -0,0 +1,19 @@
+import {useRef, useEffect} from 'react';
+import {Animated} from 'react-native';
+
+const useFadeInAnimation = ({duration = 800}: {duration?: number}) => {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: duration,
+      useNativeDriver: true,
+    }).start();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {opacity};
+};
+
+export default useFadeInAnimation;
