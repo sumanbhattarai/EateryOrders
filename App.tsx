988b1174660fe621b915e5a7924c471b05dd2e58
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-04-22 23:23:32 +0545

    Setup vector icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import Fonts from 'utils/Fonts';
 
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Icon name="keyboard-backspace" size={30} color="black" />
       <Text style={{fontFamily: Fonts.bold, fontSize: 24}}>Bold</Text>
       <Text style={{fontFamily: Fonts.regular, fontSize: 24}}>Regular</Text>
       <Text style={{fontFamily: Fonts.light, fontSize: 24}}>Light</Text>
