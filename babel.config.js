eafb40d16be22e40349a768c8de3aa4517a1ee99
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-04-22 21:36:01 +0545

    Setup module resolver and eslint

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          components: './src/components',
+          screens: './src/screens',
+          assets: './src/assets',
+          navigators: './src/navigators',
+          utils: './src/utils',
+          store: './src/store',
+          animation: './src/animation',
+          hooks: './src/hooks',
+        },
+      },
+    ],
+  ],
 };
