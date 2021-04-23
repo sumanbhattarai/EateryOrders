5d7a26958c1dd00a5154ab84b67cdd4aea13fb28
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-04-23 08:49:36 +0545

    Use new text component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+
+import Text from 'components/Text';
 
 const App = () => {
   return (
-    <View>
-      <Text>Eatery Orders</Text>
+    <View style={styles.container}>
+      <Text type="heading">Eatery Orders</Text>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
