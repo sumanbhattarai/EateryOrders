f138e7f6c9c7da3c957276d69b3b6be8c497484d
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-04-24 20:44:26 +0545

    Use onboard screen

--- App.tsx
+++ App.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
 
-import Text from 'components/Text';
+import OnBoarding from 'screens/OnBoarding';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text type="heading">Eatery Orders</Text>
-    </View>
-  );
+  return <OnBoarding />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
