0744c84e3cdf50a85a2975b848b805e339e846c6
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-04-25 20:31:57 +0545

    Add toast file

---
+++ src/utils/Toast.ts
@@ -0,0 +1,48 @@
+import {showMessage, MessageOptions} from 'react-native-flash-message';
+
+import Fonts from 'utils/Fonts';
+
+const customStyling: Partial<MessageOptions> = {
+  floating: true,
+  titleStyle: {
+    fontFamily: Fonts.bold,
+  },
+  textStyle: {
+    fontFamily: Fonts.regular,
+  },
+};
+
+const showSuccess = (options: MessageOptions) =>
+  showMessage({
+    ...options,
+    ...customStyling,
+    type: 'success',
+  });
+
+const showError = (options: MessageOptions) =>
+  showMessage({
+    ...options,
+    ...customStyling,
+    type: 'danger',
+  });
+
+const showWarning = (options: MessageOptions) =>
+  showMessage({
+    ...options,
+    ...customStyling,
+    type: 'warning',
+  });
+
+const showInfo = (options: MessageOptions) =>
+  showMessage({
+    ...options,
+    ...customStyling,
+    type: 'info',
+  });
+
+export default {
+  showError,
+  showSuccess,
+  showInfo,
+  showWarning,
+};
