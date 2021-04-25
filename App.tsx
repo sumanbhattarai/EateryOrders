65d1f8eeaf6af298d0b7a6635d69f0b89db9f5f7
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-04-25 20:31:43 +0545

    Add flash message

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import FlashMessage from 'react-native-flash-message';
 
 import MainFlow from 'screens/';
 
 const App = () => {
-  return <MainFlow />;
+  return (
+    <>
+      <MainFlow />
+      <FlashMessage />
+    </>
+  );
 };
 
 export default App;
