e6419548e455ed8d63203a550460cf4f4e1c720a
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-05-09 12:35:06 +0545

    Use separating styling file

---
+++ src/screens/Menu/styles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+import {wp} from 'utils/Constants';
+import Colors from 'utils/Colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: wp(2),
+    backgroundColor: Colors.white,
+  },
+});
+
+export default styles;
