0a5029cf25e1826ff31585acb722bee14993a6e7
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-05-19 19:21:40 +0545

    Cleanup action

--- src/store/slices/category.ts
+++ src/store/slices/category.ts
@@ -1,52 +1,64 @@
 import {
+  createAsyncThunk,
   createEntityAdapter,
   createSlice,
   PayloadAction,
 } from '@reduxjs/toolkit';
-import {Dispatch} from 'react';
 
 import {apiGetCategory} from 'api/method/category';
 import {ICategory} from 'api/utils';
-import {RootState} from 'store/';
+import {RootState} from 'store/index';
+import {RequestStatus} from 'store/utils';
 
 const categoryAdaptor = createEntityAdapter<ICategory>({
   selectId: (item) => item._id,
 });
 
-const initialState = categoryAdaptor.getInitialState({loading: false});
+const initialState = categoryAdaptor.getInitialState<{status: RequestStatus}>({
+  status: RequestStatus.Idle,
+});
+
+const fetchCategory = createAsyncThunk(
+  'category/fetch',
+  async () => {
+    const response = await apiGetCategory();
+    if (!response.success) {
+      throw Error();
+    }
+    return response.data as Array<ICategory>;
+  },
+  {
+    condition: (_, {getState}) => {
+      const rootState = getState() as RootState;
+      const {status} = rootState.category;
+      if (status === RequestStatus.Pending) {
+        return false;
+      }
+      return true;
+    },
+  },
+);
 
 const categorySlice = createSlice({
   name: 'category',
   initialState,
-  reducers: {
-    toggleLoading: (state) => {
-      state.loading = !state.loading;
-    },
-    fetchAll: (state, action: PayloadAction<ICategory[]>) => {
-      categoryAdaptor.addMany(state, action.payload);
-      state.loading = false;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategory.pending, (state) => {
+      state.status = RequestStatus.Pending;
+    });
+    builder.addCase(
+      fetchCategory.fulfilled,
+      (state, {payload: data}: PayloadAction<Array<ICategory>>) => {
+        categoryAdaptor.upsertMany(state, data);
+        state.status = RequestStatus.Fulfilled;
+      },
+    );
+    builder.addCase(fetchCategory.rejected, (state) => {
+      state.status = RequestStatus.Rejected;
+    });
   },
 });
 
 export default categorySlice.reducer;
-const {fetchAll, toggleLoading} = categorySlice.actions;
-
-const fetchCategory = () => async (
-  dispatch: Dispatch<any>,
-  getState: () => RootState,
-) => {
-  const {loading} = getState().category;
-  if (loading) {
-    return;
-  }
-  dispatch(toggleLoading());
-  const response = await apiGetCategory();
-  if (response.success) {
-    dispatch(fetchAll(response.data!));
-  } else {
-    dispatch(toggleLoading());
-  }
-};
-
 export {fetchCategory};
