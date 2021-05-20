6b4e2ba026d00711544d1f47c47df83a38a03db4
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-05-20 09:06:03 +0545

    Add few mapping functions

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,3 +1,5 @@
+import {IFoodItem, IMenu} from 'api/utils';
+
 /* eslint-disable no-shadow */
 export enum RequestStatus {
   Idle = 'idle',
@@ -5,3 +7,24 @@ export enum RequestStatus {
   Fulfilled = 'fulfilled',
   Rejected = 'rejected',
 }
+
+export interface IMappedMenus {
+  category: string;
+  items: Array<string>;
+}
+
+export const getFoodItemArray = (data: Array<IMenu>) => {
+  let foodItems: Array<IFoodItem> = [];
+  for (const category in data) {
+    for (const item in data[(category as any) as number].item) {
+      foodItems = [...foodItems, data[(category as any) as number].item[item]];
+    }
+  }
+  return foodItems;
+};
+
+export const getIdMappedMenus = (data: Array<IMenu>) =>
+  data.map((el) => {
+    const ids = el.item.map((item) => item._id);
+    return ({...el, item: ids} as any) as IMappedMenus;
+  });
