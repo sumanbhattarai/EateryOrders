43ed6de1cdd8b115d1e9c3dc50816705b41af0f9
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-06-01 20:48:40 +0545

    Change parameter

--- src/api/method/category.ts
+++ src/api/method/category.ts
@@ -1,11 +1,11 @@
 import {makeRequest, Routes} from 'api/';
 import {IApiResponse, ICategory} from 'api/utils';
 
-const apiAddCategory = (data: {name: string}) =>
+const apiAddCategory = (name: string) =>
   makeRequest<IApiResponse<ICategory>>({
     url: Routes.AddCategory,
     method: 'POST',
-    data,
+    data: {name},
   });
 
 const apiGetCategory = () =>
