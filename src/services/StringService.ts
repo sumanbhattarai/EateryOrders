280fd2fd8d5f16c85d762a6d2a712b513dee9ac9
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2021-06-02 19:58:26 +0545

    Add string management function

--- src/services/StringService.ts
+++ src/services/StringService.ts
@@ -1,4 +1,9 @@
 const makeFirstCharCapital = (c: string): string =>
   c.charAt(0).toUpperCase() + c.slice(1);
 
-export {makeFirstCharCapital};
+/* This func takes the string and return string that has first letter capital along with no
+whitespaces at start & end as well as no extra whitespaes between the word. */
+const properStringValue = (c: string): string =>
+  makeFirstCharCapital(c.trim().replace(/ +/g, ' '));
+
+export {makeFirstCharCapital, properStringValue};
