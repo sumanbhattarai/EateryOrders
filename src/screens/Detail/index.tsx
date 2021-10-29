7c9538c33db9fa7b77ce3802a68150eed57c27da
Suman Bhattarai <sumanbhattaraiofficial@gmail.com>
2021-10-29 15:52:06 +0545

    Change detail screen ui

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, ScrollView} from 'react-native';
 import {RouteProp} from '@react-navigation/native';
 
+import styles from './styles';
 import {useAppSelector} from 'services/TypedRedux';
 import {RootStackParamList} from 'navigators/utils';
+import Text from 'components/Text';
+import Colors from 'utils/Colors';
 
 interface Props {
   route: RouteProp<RootStackParamList, 'FoodDetail'>;
@@ -14,11 +17,58 @@ const Detail = ({route}: Props) => {
   const id = route.params.id;
   const {name, category, price} = entities[id]!;
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{category}</Text>
-      <Text>{price}</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={require('assets/images/pizza.png')} style={styles.image} />
+      <View style={styles.informationBox}>
+        <Text type="heading" style={styles.heading}>
+          {name}
+        </Text>
+        <View style={styles.horizontalFlex}>
+          <Text color={Colors.grey}>{category}</Text>
+          <Text color={Colors.grey}>Rs.{price}</Text>
+        </View>
+        <View style={styles.detail}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+            corrupti, commodi maxime officia odio asperiores temporibus quos
+            omnis ut necessitatibus minus quam, molestiae vero obcaecati optio
+            accusamus mollitia quasi libero! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Explicabo assumenda, reiciendis eius
+            totam et officia doloribus autem delectus maxime ex quidem natus id
+            aperiam. Cupiditate quaerat culpa inventore consequatur
+            necessitatibus? Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Dolorum blanditiis minus officiis facere, reprehenderit
+            voluptates totam neque, dolores odio aliquam natus, nam assumenda
+            nobis fugit dolor explicabo cupiditate quibusdam? Porro. {'\n'}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            tempore nemo incidunt aliquid maiores ad ex at laudantium, veritatis
+            accusantium libero sapiente quo officiis odio iure voluptatum
+            eveniet nihil? Sapiente. Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Rerum cupiditate iure et ea facilis aliquid
+            distinctio ab itaque! Rerum debitis sapiente, porro soluta alias
+            quis odio incidunt iusto nesciunt obcaecati. {'\n'} {'\n'} Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+            corrupti, commodi maxime officia odio asperiores temporibus quos
+            omnis ut necessitatibus minus quam, molestiae vero obcaecati optio
+            accusamus mollitia quasi libero! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Explicabo assumenda, reiciendis eius
+            totam et officia doloribus autem delectus maxime ex quidem natus id
+            aperiam. Cupiditate quaerat culpa inventore consequatur
+            necessitatibus? Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Dolorum blanditiis minus officiis facere, reprehenderit
+            voluptates totam neque, dolores odio aliquam natus, nam assumenda
+            nobis fugit dolor explicabo cupiditate quibusdam? Porro. {'\n'}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            tempore nemo incidunt aliquid maiores ad ex at laudantium, veritatis
+            accusantium libero sapiente quo officiis odio iure voluptatum
+            eveniet nihil? Sapiente. Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Rerum cupiditate iure et ea facilis aliquid
+            distinctio ab itaque! Rerum debitis sapiente, porro soluta alias
+            quis odio incidunt iusto nesciunt obcaecati.
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ src/screens/Detail/styles.ts
@@ -0,0 +1,37 @@
+import {ImageStyle, StyleSheet, TextStyle, ViewStyle} from 'react-native';
+
+import {hp, Width, wp} from 'utils/Constants';
+
+interface Style {
+  container: ViewStyle;
+  image: ImageStyle;
+  informationBox: ViewStyle;
+  heading: TextStyle;
+  horizontalFlex: ViewStyle;
+  detail: ViewStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: Width,
+    height: Width / 1.25,
+  },
+  informationBox: {
+    padding: wp(2),
+  },
+  heading: {
+    textAlign: 'center',
+  },
+  horizontalFlex: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  detail: {
+    marginTop: hp(2),
+  },
+});
+
+export default styles;
