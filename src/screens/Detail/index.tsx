393b2e476bf77929f3a5c0ed94a3857d98926864
Suman Bhattarai <sumanbhattaraiofficial@gmail.com>
2021-10-29 14:34:23 +0545

    Type checking route prop

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {RouteProp} from '@react-navigation/native';
 
-const Detail = () => {
+import {useAppSelector} from 'services/TypedRedux';
+import {RootStackParamList} from 'navigators/utils';
+
+interface Props {
+  route: RouteProp<RootStackParamList, 'FoodDetail'>;
+}
+
+const Detail = ({route}: Props) => {
+  const {entities} = useAppSelector((state) => state.menu);
+  const id = route.params.id;
+  const {name, category, price} = entities[id]!;
   return (
     <View>
-      <Text>Detail Screen</Text>
+      <Text>{name}</Text>
+      <Text>{category}</Text>
+      <Text>{price}</Text>
     </View>
   );
 };
