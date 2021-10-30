ad4bf3d016546c5c5531097fd2364212f87c5c6e
Suman Bhattarai <sumanbhattaraiofficial@gmail.com>
2021-10-30 14:20:57 +0545

    Initial commit

---
+++ src/screens/AddCategory/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AddCategory = () => {
+  return (
+    <View>
+      <Text>Add Food</Text>
+    </View>
+  );
+};
+
+export default AddCategory;
