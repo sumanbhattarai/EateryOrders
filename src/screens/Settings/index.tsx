22ff595211b8885de812bbc7bb499c3a480f81fd
Suman Bhattarai <sumanbhattaraiofficial@gmail.com>
2021-10-30 14:21:24 +0545

    Initial commit

---
+++ src/screens/Settings/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import Text from 'components/Text';
+
+const Settings = () => {
+  return (
+    <View>
+      <Text> Settings</Text>
+    </View>
+  );
+};
+
+export default Settings;
