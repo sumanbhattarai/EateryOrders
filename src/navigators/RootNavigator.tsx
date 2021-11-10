b96d080f4fc595ed49626b4e0412c91fcf143468
Suman Bhattarai <sumanbhattaraiofficial@gmail.com>
2021-11-11 02:54:46 +0545

    Fix SafeAreaView glitch

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Entypo';
-import {SafeAreaView} from 'react-native-safe-area-context';
 
 import AddFood from 'screens/AddFood';
 import AddCategory from 'screens/AddCategory';
