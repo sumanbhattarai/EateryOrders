4ffc79699473d37c1cb54d1910ceac89e2a83d1a
Suman Bhattarai <sumanbhattaraiofficial@gmail.com>
2021-11-11 03:07:21 +0545

    Initial commit

--- src/screens/Delivered/index.tsx
+++ src/screens/Delivered/index.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect} from 'react';
+import {FlatList, RefreshControl} from 'react-native';
+import ContentLoader from 'react-native-easy-content-loader';
 
-import Text from 'components/Text';
+import styles from './styles';
+import OrderCard from 'components/OrderCard';
+import {useAppSelector, useAppDispatch} from 'services/TypedRedux';
+import {getOrder} from 'store/slices/order';
+import {RequestStatus} from 'store/utils';
+import {hp} from 'utils/Constants';
 
 const Delivered = () => {
+  const {status, entities, ids} = useAppSelector((state) => state.order);
+  const dispatch = useAppDispatch();
+  const isLoading = status === RequestStatus.Pending;
+  const inReviewIds = ids.filter((el) => entities[el]?.status === 'Confirmed');
+
+  useEffect(() => {
+    dispatch(getOrder());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <View>
-      <Text>DELIVERED</Text>
-    </View>
+    <ContentLoader
+      loading={isLoading && ids.length < 1}
+      listSize={6}
+      pRows={1}
+      pHeight={hp(14)}
+      pWidth={'100%'}
+      tHeight={0}
+      animationDuration={1000}
+      containerStyles={styles.cardContainer}>
+      <FlatList
+        data={inReviewIds}
+        keyExtractor={(item, index) => `${entities[item]?._id}-${index}`}
+        refreshControl={
+          <RefreshControl
+            refreshing={isLoading && ids.length > 0}
+            onRefresh={() => dispatch(getOrder())}
+          />
+        }
+        renderItem={({item}) => <OrderCard id={item} />}
+      />
+    </ContentLoader>
   );
 };
 
---
+++ src/screens/Delivered/styles.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+import {wp} from 'utils/Constants';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: wp(2),
+    paddingBottom: 0,
+  },
+  fullFlex: {
+    flex: 1,
+  },
+  cardContainer: {
+    marginTop: wp(2),
+  },
+});
+
+export default styles;
